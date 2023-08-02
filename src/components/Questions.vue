44a10a3349f006c54a8312c933dd17bf83c3df84
karyna.kananovich <karyna4k.dev@gmail.com>
2023-08-02 10:47:06 +0400

    feat: update Questions component

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="questions-ctr">
     <div class="progress">
-      <div class="bar"></div>
-      <div class="status">1 out of 3 questions answered</div>
-    </div>
-    <div
-      class="single-question"
-      v-for="question in questions"
-      :key="question.q"
-    >
-      <div class="question">{{ question.q }}</div>
       <div
-        class="answers"
-        v-for="answer in question.answers"
-        :key="answer.text"
-      >
-        <div class="answer">{{ answer.text }}</div>
+        class="bar"
+        :style="{ width: `${getStatusBarWidth}%` }"
+      ></div>
+      <div class="status">
+        {{ questionsAnswered }} out of {{ questions.length }} questions answered
       </div>
     </div>
+    <transition-group name="fade">
+      <div
+        class="single-question"
+        v-for="(question, qi) in questions"
+        :key="question.q"
+        v-show="questionsAnswered === qi"
+      >
+        <div class="question">{{ question.q }}</div>
+        <div
+          class="answers"
+          v-for="answer in question.answers"
+          :key="answer.text"
+          @click.prevent="selectAnswer(answer.is_correct)"
+        >
+          <div class="answer">{{ answer.text }}</div>
+        </div>
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -27,6 +36,21 @@ export default {
     questions: {
       type: Array,
       required: true,
+    },
+    questionsAnswered: {
+      type: Number,
+      required: true,
+    },
+  },
+  emits: ['select-answer'],
+  computed: {
+    getStatusBarWidth() {
+      return (this.questionsAnswered / this.questions.length) * 100;
+    },
+  },
+  methods: {
+    selectAnswer(isCorrect) {
+      this.$emit('select-answer', isCorrect);
     },
   },
 };
